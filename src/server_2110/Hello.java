5ecbf20bbac03e44e66d065ab25765c1b83b7376
ttttkii913 <dbgkdud821@daum.net>
2024-04-24 00:39:37 +0900

    202214033-5

--- src/server_2110/Hello.java
+++ src/server_2110/Hello.java
@@ -2,8 +2,8 @@ package server_2110;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("202214033");
-		System.out.println("유하영");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 202214033");
+		System.out.println("이름: 유하영");
 	}
 }
